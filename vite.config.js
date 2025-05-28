89872273c60cef0ffbdccb936f2ac2152721af46
ira lux <nucleareactive@gmail.com>
2025-05-28 12:44:20 +0800

    Fix Vite base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/flex-explorer/', // e.g., '/axle-flex-explorer/'
+  base: '/', 
   plugins: [react()],
 })
 
